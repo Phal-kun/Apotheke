ce6a2f504a003edeb387fa8b076e7c266799c361
hiepdev3 <169441533+warrendevf@users.noreply.github.com>
2024-09-15 22:16:16 +0700

    Update project.properties
    dcx

---
+++ sql/Database script.sql
@@ -0,0 +1,156 @@
+--create database SWP
+-- drop database SWP391
+
+CREATE TABLE [user] (
+  [userID] INT PRIMARY KEY,
+  [fullname] NVARCHAR(255),
+  [phone] NVARCHAR(255),
+  [username] NVARCHAR(255),
+  [password] NVARCHAR(255),
+  [gender] NVARCHAR(255),
+  [status] BIT,
+  [role] INT,
+  [address] NVARCHAR(255)
+);
+GO
+
+CREATE TABLE [role] (
+  [roleID] INT PRIMARY KEY,
+  [roleName] NVARCHAR(255)
+);
+GO
+
+CREATE TABLE [product] (
+  [productID] INT PRIMARY KEY,
+  [productName] NVARCHAR(255),
+  [CategoryID] INT,
+  [originID] INT,
+  [MeasureUnitID] INT,
+  [ManufacturerID] INT,
+  [FormID] INT,
+  [Description] NVARCHAR(255),
+  [ImportDate] DATE,
+  [ExpiredDate] DATE
+);
+GO
+
+CREATE TABLE [Component] (
+  [componentID] INT PRIMARY KEY,
+  [componentName] NVARCHAR(255)
+);
+GO
+
+CREATE TABLE [ComponentProduct] (
+  [componentID] INT,
+  [productID] INT,
+  [quantity] INT,
+  PRIMARY KEY ([componentID], [productID])
+);
+GO
+
+CREATE TABLE [Origin] (
+  [originID] INT PRIMARY KEY,
+  [originName] NVARCHAR(255)
+);
+GO
+
+CREATE TABLE [ProductUnit] (
+  [productUnitID] INT PRIMARY KEY,
+  [productUnitName] NVARCHAR(255)
+);
+GO
+
+CREATE TABLE [Form] (
+  [formID] INT PRIMARY KEY,
+  [formName] NVARCHAR(255)
+);
+GO
+
+CREATE TABLE [Manufacturer] (
+  [manufacturerID] INT PRIMARY KEY,
+  [manufacturerName] NVARCHAR(255)
+);
+GO
+
+CREATE TABLE [Category] (
+  [CategoryID] INT PRIMARY KEY,
+  [ParentCategoryID] INT,
+  [CategoryName] NVARCHAR(255),
+  [Description] NVARCHAR(255)
+);
+GO
+
+CREATE TABLE [productDetail] (
+  [productDetailID] INT PRIMARY KEY,
+  [productID] INT,
+  [productUnitID] INT,
+  [volume] INT,
+  [stock] INT,
+  [price] INT
+);
+GO
+
+CREATE TABLE [blog] (
+  [blogID] INT PRIMARY KEY,
+  [title] NVARCHAR(255),
+  [content] NVARCHAR(MAX),
+  [publicDate] DATE,
+  [userID] INT,
+  [status] BIT
+);
+GO
+
+CREATE TABLE [tag] (
+  [tagID] INT PRIMARY KEY,
+  [tagName] NVARCHAR(255)
+);
+GO
+
+CREATE TABLE [blogTag] (
+  [blogID] INT,
+  [tagID] INT,
+  PRIMARY KEY ([blogID], [tagID])
+);
+GO
+
+-- Adding foreign keys
+ALTER TABLE [user] ADD FOREIGN KEY ([role]) REFERENCES [role] ([roleID]);
+GO
+
+ALTER TABLE [product] ADD FOREIGN KEY ([originID]) REFERENCES [Origin] ([originID]);
+GO
+
+ALTER TABLE [product] ADD FOREIGN KEY ([ManufacturerID]) REFERENCES [Manufacturer] ([manufacturerID]);
+GO
+
+ALTER TABLE [Category] ADD FOREIGN KEY ([ParentCategoryID]) REFERENCES [Category] ([CategoryID]);
+GO
+
+ALTER TABLE [product] ADD FOREIGN KEY ([CategoryID]) REFERENCES [Category] ([CategoryID]);
+GO
+
+ALTER TABLE [product] ADD FOREIGN KEY ([FormID]) REFERENCES [Form] ([formID]);
+GO
+
+ALTER TABLE [productDetail] ADD FOREIGN KEY ([productID]) REFERENCES [product]([productID]);
+GO
+
+ALTER TABLE [productDetail] ADD FOREIGN KEY ([productUnitID]) REFERENCES [productUnit]([productUnitID]);
+GO
+
+-- Component-Product relations
+ALTER TABLE [ComponentProduct] ADD FOREIGN KEY ([componentID]) REFERENCES [Component] ([componentID]);
+GO
+
+ALTER TABLE [ComponentProduct] ADD FOREIGN KEY ([productID]) REFERENCES [product] ([productID]);
+GO
+
+-- Blog relations
+ALTER TABLE [blog] ADD FOREIGN KEY ([userID]) REFERENCES [user] ([userID]);
+GO
+
+ALTER TABLE [blogTag] ADD FOREIGN KEY ([blogID]) REFERENCES [blog] ([blogID]);
+GO
+
+ALTER TABLE [blogTag] ADD FOREIGN KEY ([tagID]) REFERENCES [tag] ([tagID]);
+GO
---
+++ src/java/Model/Product/Category.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.Product;
+
+import java.util.Objects;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Category {
+    private int categoryID;
+    private Category parentCategory;
+    private String categoryName;
+    private String description;
+
+    public Category() {
+    }
+
+    public Category(int categoryID, Category parentCategory, String categoryName, String description) {
+        this.categoryID = categoryID;
+        this.parentCategory = parentCategory;
+        this.categoryName = categoryName;
+        this.description = description;
+    }
+    
+    private static final Logger LOG = Logger.getLogger(Category.class.getName());
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public Category getParentCategory() {
+        return parentCategory;
+    }
+
+    public void setParentCategory(Category parentCategory) {
+        this.parentCategory = parentCategory;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + this.categoryID;
+        hash = 59 * hash + Objects.hashCode(this.parentCategory);
+        hash = 59 * hash + Objects.hashCode(this.categoryName);
+        hash = 59 * hash + Objects.hashCode(this.description);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Category other = (Category) obj;
+        if (this.categoryID != other.categoryID) {
+            return false;
+        }
+        if (!Objects.equals(this.categoryName, other.categoryName)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        return Objects.equals(this.parentCategory, other.parentCategory);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" + "categoryID=" + categoryID + ", parentCategory=" + parentCategory + ", categoryName=" + categoryName + ", description=" + description + '}';
+    }
+    
+    
+    
+}
