d2f9d35624498256acaa0022ab56c5df212b0cc6
hiepdev3 <169441533+warrendevf@users.noreply.github.com>
2024-09-25 02:01:50 +0700

    d

---
+++ src/java/DAL/UserDao.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAL;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+/**
+ *
+ * @author Dell
+ */
+public class UserDao {
+    java.sql.Connection con= null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+}
