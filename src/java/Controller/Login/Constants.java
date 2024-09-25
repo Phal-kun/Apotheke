e44e9a65eeae6232da9de51564ef3d0cdad66799
Phalkun <lehuydiep4@gmail.com>
2024-09-25 09:51:44 +0700

    Fix Google login

--- src/java/Controller/Login/Constants.java
+++ src/java/Controller/Login/Constants.java
@@ -9,9 +9,9 @@ package Controller.Login;
  * @author Dell
  */
 public class Constants {
-    public static final String GOOGLE_CLIENT_ID ="281219533244-p3s850ncnfsj1lhkh49vc21nvjmo2tjh.apps.googleusercontent.com";
-    public static final String GOOGLE_CLIENT_SECRET="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";
-    public static final String GOOGLE_REDIRECT_URL="http://localhost:9998/Apotheke/loginGoogle";
+    public static final String GOOGLE_CLIENT_ID ="774397983057-vbou6cr3kmos51u2c552btnlt4l0fh5t.apps.googleusercontent.com";
+    public static final String GOOGLE_CLIENT_SECRET="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";
+    public static final String GOOGLE_REDIRECT_URL="http://localhost:8080/Apotheke/loginGoogle";
     public static final String GOOGLE_GRANT_TYPE="authorization_code";
     public static final String GOOGLE_LINK_GET_TOKEN="https://accounts.google.com/o/oauth2/token";
     public static final String GOOGLE_LINK_GET_USER_INFO="https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses,genders,photos,birthdays,phoneNumbers";
