b842f189871575a79f3aaecb1e3e37cd4a40c00e
Phalkun <lehuydiep4@gmail.com>
2024-10-12 22:31:11 +0700

    Update model

--- src/java/Model/Order/Status.java
+++ src/java/Model/Order/Status.java
@@ -4,11 +4,83 @@
  */
 package Model.Order;
 
+import java.util.Objects;
+import java.util.logging.Logger;
+
 /**
  *
  * @author ASUS
  */
 public class Status {
-    int statusID;
-    String statusName, description;
+    private int statusID;
+    private String statusName, description;
+
+    public Status() {
+    }
+    private static final Logger LOG = Logger.getLogger(Status.class.getName());
+
+    public int getStatusID() {
+        return statusID;
+    }
+
+    public void setStatusID(int statusID) {
+        this.statusID = statusID;
+    }
+
+    public String getStatusName() {
+        return statusName;
+    }
+
+    public void setStatusName(String statusName) {
+        this.statusName = statusName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public static Logger getLOG() {
+        return LOG;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 47 * hash + this.statusID;
+        hash = 47 * hash + Objects.hashCode(this.statusName);
+        hash = 47 * hash + Objects.hashCode(this.description);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Status other = (Status) obj;
+        if (this.statusID != other.statusID) {
+            return false;
+        }
+        if (!Objects.equals(this.statusName, other.statusName)) {
+            return false;
+        }
+        return Objects.equals(this.description, other.description);
+    }
+
+    @Override
+    public String toString() {
+        return "Status{" + "statusID=" + statusID + ", statusName=" + statusName + ", description=" + description + '}';
+    }
+    
+    
 }
